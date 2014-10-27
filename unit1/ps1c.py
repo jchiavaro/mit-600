dfb8e16cd0c6cde1f5881a77ac99cb7547e3030c
jchiavaro <jorgechiavaro@gmail.com>
2014-10-27 20:41:49 -0300

    PS1-C: CC Payment with bisection search

--- unit1/ps1c.py
+++ unit1/ps1c.py
@@ -1,17 +1,34 @@
 balance = float(raw_input("Enter the outstanding balance on your credit card: "))
 annual_cc_interest = float(raw_input("Enter the annual credit card interest rate as a decimal: "))
 
+low = balance / 12
+high =  (balance * (1 + (annual_cc_interest / 12)) ** 12) / 12
 min_monthly_payment = 0
-remaining_balance = 0
-monthly_interest = annual_cc_interest / 12.0
-month = 1
-low = balance / 12.0
-high = (balanceBalance * (1 + (annual_cc_interest / 12.0)) ** 12.0) / 12.0
+while(True):
+    remaining_balance = balance
+    min_monthly_payment = (low + high)/2.0
+    for i in range(1, 13):
+        interest = round(balance * annual_cc_interest / 12, 2)
+        remaining_balance +=  interest - min_monthly_payment
+        if(remaining_balance <= 0):
+            break
 
-while(abs(remaining_balance - )):
-    min_monthly_payment = min_monthly_payment + 0.01
+    if(high-low < 0.005 ):
+        print "RESULT"
+        min_monthly_payment = round(min_monthly_payment + 0.004999, 2)
+        print "Monthly payment to pay off debt in 1 year:", round(min_monthly_payment, 2)
 
-print("RESULT")
-print("Monthly payment to pay off debt in 1 year: " + str(min_monthly_payment))
-print("Number of months needed: " + str(month))
-print("Balance: " + str(remaining_balance))
+        remaining_balance = balance
+
+        for month in range(1,13):
+            interest = round(balance * annual_cc_interest / 12, 2)
+            remaining_balance += interest - min_monthly_payment
+            if balance <= 0:
+                break
+        print "Number of months needed:", month
+        print "Balance:", round(remaining_balance, 2)
+        break
+    elif remaining_balance < 0:
+        high = min_monthly_payment
+    else:
+        low = min_monthly_payment
